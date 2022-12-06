303b3ee02c70f28948f855a446abc2447ff8f702
Joshua Fielding <josh.e.fielding@gmail.com>
2022-12-07 06:57:59 +1100

    added exercise 3 - step 1

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -1,36 +1,50 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core'
-
+import * as React from 'react'
 import './bootstrap'
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from 'utils/api-client.exercise'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [status, setStatus] = React.useState('idle')
+  const [query, setQuery] = React.useState('')
+  const [data, setData] = React.useState(null)
+  // don't want to run the search until user has submitted the form,
+  // this boolean is for that
+  const [queried, setQueried] = React.useState(false)
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  const isLoading = status === 'loading'
+  const isSuccess = status === 'success'
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  // useEffect makes the request with the client,
+  // and updates the status and data
+  // remember react calls the callback immediately, so add dependency list
+  React.useEffect(() => {
+    // if already queried, return early, because effect callbacks are called on
+    // the initial render too
+    if (!queried) {
+      return
+    }
+    setStatus('loading')
+    // window
+    //   .fetch('https://dummyjson.com/products/1')
+    //   .then(response => response.json())
+    //   .then(json => console.log(json))
+
+    client(`books?query=${encodeURIComponent(query)}`).then(responseData => {
+      setData(responseData)
+      setStatus('success')
+    })
+  }, [queried, query])
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault() // prevent full page reload
+    setQuery(event.target.elements.search.value) // set the query value
+    // console.log(event.target.elements.search.value)
+    setQueried(true)
   }
 
   return (
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,108 +1,14 @@
-function client(endpoint, customConfig = {}) {
-  // 🐨 create the config you'll pass to window.fetch
-  //    make the method default to "GET"
-  // 💰 if you're confused by this, that's fine. Scroll down to the bottom
-  // and I've got some code there you can copy/paste.
-  // 🐨 call window.fetch(fullURL, config) then handle the json response
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-  // 💰 here's how to get the full URL: `${process.env.REACT_APP_API_URL}/${endpoint}`
-}
-
-export {client}
-
-/*
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-💰 spoiler alert below...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const config = {
+async function client(endpoint, customConfig = {}) {
+  // create the config to pass to window.fetch
+  // make the method default to "GET"
+  const config = {
     method: 'GET',
     ...customConfig,
   }
-*/
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  let url = `${process.env.REACT_APP_API_URL}/${endpoint}` // full URL
+  // call window,fetch(fullURL, config) then handle the JSON response
+  return window.fetch(url, config).then(response => response.json())
+}
+
+export {client}
