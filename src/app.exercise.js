0af60443b16d11c3a43315919d55e5b86dd305eb
Joshua Fielding <josh.e.fielding@gmail.com>
2022-12-13 06:39:25 +1100

    added exercise 4 - extra 1

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -5,6 +5,20 @@ import * as React from 'react'
 import * as auth from 'auth-provider'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {client} from 'utils/api-client.extra-4'
+
+const getUser = async () => {
+  let user = null
+  const token = await auth.getToken()
+
+  if (token) {
+    // we're logged in, so let's go get the user's data
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
   // set state for the user
@@ -17,7 +31,14 @@ function App() {
   const register = form => auth.register(form).then(u => setUser(u))
 
   // create a logout function that calls auth.logout() and sets the user to null
-  const logout = () => auth.logout().then(setUser(null))
+  const logout = () => {
+    auth.logout()
+    setUser(null)
+  }
+
+  React.useEffect(() => {
+    getUser().then(u => setUser(u))
+  }, []) // pass dependency so only runs once on mount
 
   // if there's a user, then render the AuthenticatedApp with the user and logout,
   // if not, render UnauthenticatedApp with the login and register
