03f67dd8a80f1b8eca7789a7bfb03473ed90853f
Joshua Fielding <josh.e.fielding@gmail.com>
2022-12-14 06:17:17 +1100

    added exercise 4 - extra 3 and 4

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,11 +1,20 @@
+import * as auth from 'auth-provider'
+
 const apiURL = process.env.REACT_APP_API_URL
+
+// if there is data being sent, it's a POST, so that's how we handle the ternaries
 
 function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {data, token, method, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    // handle the method
+    method: data ? 'POST' : 'GET',
+    // serialise the data, handle the body
+    body: data ? JSON.stringify(data) : undefined,
     headers: {
+      'Content-Type': data ? 'application/json' : undefined,
       Authorization: token ? `Bearer ${token}` : undefined,
       ...customHeaders,
     },
@@ -14,6 +23,11 @@ function client(
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
     const data = await response.json()
+    if (response.status === 401) {
+      await auth.logout() // log user out
+      window.location.assign(window.location) // clear state of the app, wipe memory clean, send user back to homepage
+      return Promise.reject({message: 'Please re-authenticate'})
+    }
     if (response.ok) {
       return data
     } else {
