99c0a057f628883ddab148a09133fa560e4ad4a9
Joshua Fielding <josh.e.fielding@gmail.com>
2023-01-06 06:55:26 +1100

    added exercise 10 - steps 1 and 2

--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -1,6 +1,6 @@
-export * from './auth-context.final'
+// export * from './auth-context.final'
 
-// export * from './auth-context.exercise'
+export * from './auth-context.exercise'
 
 // 💯 Preload all initial data
 // export * from './auth-context.extra-1'
